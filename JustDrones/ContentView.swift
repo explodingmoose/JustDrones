190231b41d10aa6eccf3cad3ee6d2c33a50c2a96
Eli Pouliot <ejhpouliot@gmail.com>
2025-06-02 09:13:08 -0600

    Refactoring Content View body and New App Icon

--- JustDrones/ContentView.swift
+++ JustDrones/ContentView.swift
@@ -32,62 +32,71 @@ struct ContentView: View {
     @SceneStorage("ContentView.displayMode") private var displayMode = DisplayMode.noteName
     @SceneStorage("ContentView.tuningMode") private var tuningMode = TuningMode.Tonnetz
     
+    var drones: some View {
+        VStack(spacing: 5) {
+            switch tuningMode {
+            case .Tonnetz:
+                Tonnetz(displayMode: displayMode, synth: theSynth, recorder: theRecorder, droneManager: theDroneManager)
+            case .CircleFifths:
+                CircleOfFifths(displayMode: displayMode, synth: theSynth, recorder: theRecorder, droneManager: theDroneManager)
+            case .Recorded:
+                Recorded(diapason: 440, stop: 16, displayMode: displayMode, recorder: theRecorder, synth: theSynth, isPedal: isPedal)
+                    .onAppear() {theRecorder.recording = false}
+            }
+        }
+    }
+    var buttons: some View {
+        HStack(alignment: .center, spacing: 20) {
+            Button(action: {
+                isControlMenuOpen.toggle()
+            }) {
+                Image(systemName: "gear")
+                    .iconStyle()
+            }
+            
+            Button(action: {
+                isSynthMenuOpen.toggle()
+            }) {
+                Image(systemName: "slider.horizontal.3")
+                    .iconStyle()
+            }
+            
+            Button(action: {
+                isRandomMenuOn.toggle()
+            }) {
+                Image(systemName: "dice")
+                    .iconStyle()
+            }
+            
+            Button(action: {
+                theRecorder.recording.toggle()
+            }) {
+                    Image(systemName: "stop.circle")
+                        .font(.system(size: 24))
+                        .foregroundStyle(theRecorder.recording ? .red : Color.accentColor)
+                    
+            }
+        }
+    }
+    var menu: some View {
+        ZStack{
+            if isSynthMenuOpen {SynthMenu(synth: theSynth, isSynthMenuOpen: $isSynthMenuOpen)}
+            if isControlMenuOpen {ControlMenu(droneManager: theDroneManager, synth: theSynth, isControlMenuOpen: $isControlMenuOpen, displayMode: $displayMode, tuningMode: $tuningMode, isPedal: $isPedal)}
+            if isRandomMenuOn {RandomMenu(isRandomMenuOn: $isRandomMenuOn)}
+        }
+    }
+    
     var body: some View {
         ZStack{
             VStack {
                 //Drone views
-                VStack(spacing: 5) {
-                    switch tuningMode {
-                    case .Tonnetz:
-                        Tonnetz(displayMode: displayMode, synth: theSynth, recorder: theRecorder, droneManager: theDroneManager)
-                    case .CircleFifths:
-                        CircleOfFifths(displayMode: displayMode, synth: theSynth, recorder: theRecorder, droneManager: theDroneManager)
-                    case .Recorded:
-                            Recorded(diapason: 440, stop: 16, displayMode: displayMode, recorder: theRecorder, synth: theSynth, isPedal: isPedal)
-                                .onAppear() {theRecorder.recording = false}
-                    }
-                }
+                drones
                 //Buttons for menus
-                HStack(alignment: .center, spacing: 20) {
-                    Button(action: {
-                        isControlMenuOpen.toggle()
-                    }) {
-                        Image(systemName: "gear")
-                            .iconStyle()
-                    }
-                    
-                    Button(action: {
-                        isSynthMenuOpen.toggle()
-                    }) {
-                        Image(systemName: "slider.horizontal.3")
-                            .iconStyle()
-                    }
-                    
-                    Button(action: {
-                        isRandomMenuOn.toggle()
-                    }) {
-                        Image(systemName: "dice")
-                            .iconStyle()
-                    }
-                    
-                    Button(action: {
-                        theRecorder.recording.toggle()
-                    }) {
-                            Image(systemName: "stop.circle")
-                                .font(.system(size: 24))
-                                .foregroundStyle(theRecorder.recording ? .red : Color.accentColor)
-                            
-                    }
-                }
+                buttons
             }
-            
             //Menu displays
-            if isSynthMenuOpen {SynthMenu(synth: theSynth, isSynthMenuOpen: $isSynthMenuOpen)}
-            if isControlMenuOpen {ControlMenu(droneManager: theDroneManager, synth: theSynth, isControlMenuOpen: $isControlMenuOpen, displayMode: $displayMode, tuningMode: $tuningMode, isPedal: $isPedal)}
-            if isRandomMenuOn {RandomMenu(isRandomMenuOn: $isRandomMenuOn)}
-            
+            menu
         }
     }
-    
 }
 
